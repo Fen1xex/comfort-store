eb2ff72e855724e74edce70c6800607139aa19b0
KIRILL F <tiltlessman@gmail.com>
2022-03-11 15:34:47 +0300

    error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,44 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 const Error = () => {
-  return <h2>Error</h2>
+  return (
+    <Wrapper>
+      <div>
+        <h3>404</h3>
+        <p>Sorry, there no page matched your search</p>
+        <Link to='/'>back home</Link>
+      </div>
+    </Wrapper>
+  )
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: calc(100vh - 10rem);
+  div {
+    display: grid;
+    place-items: center;
+  }
+  h3 {
+    font-size: 5rem;
+  }
+  p {
+    color: var(--heading);
+  }
+  a {
+    color: var(--heading);
+    background: var(--background);
+    padding: 0.5rem 1rem;
+    border-radius: var(--radius);
+    &:hover {
+      transition: all 0.1s linear;
+      color: var(--primary1);
+    }
+  }
+`
 
 export default Error
