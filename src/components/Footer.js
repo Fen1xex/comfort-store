39125861a6deea30cf2b2c8d847510856b414422
KIRILL F <tiltlessman@gmail.com>
2022-03-11 14:13:04 +0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Footer = () => {
-  return <h2>Footer</h2>
+  return (
+    <Wrapper>
+      <div className='footer-center'>
+        &copy; all rigths reserved
+        <br />
+        <span>{new Date().getFullYear()}</span>
+      </div>
+    </Wrapper>
+  )
 }
+
+const Wrapper = styled.footer`
+  height: 5rem;
+  background: var(--background);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: var(--heading);
+  .footer-center {
+    display: grid;
+    place-items: center;
+  }
+`
 
 export default Footer
