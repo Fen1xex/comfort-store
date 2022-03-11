45b3ae4822629b9e601da5870829fb71d0fdca57
KIRILL F <tiltlessman@gmail.com>
2022-03-11 13:08:40 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Home, About, Error } from './pages/index'
 import { Navbar, Sidebar, Footer } from './components/index'
@@ -6,16 +7,20 @@ import { Navbar, Sidebar, Footer } from './components/index'
 const App = () => {
   return (
     <Router>
-      <Navbar />
-      <Sidebar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='*' element={<Error />} />
-      </Routes>
-      <Footer />
+      <Wrapper>
+        <Navbar />
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='*' element={<Error />} />
+        </Routes>
+        <Footer />
+      </Wrapper>
     </Router>
   )
 }
+
+const Wrapper = styled.main``
 
 export default App
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,78 @@
 import React from 'react'
+import styled from 'styled-components'
+import { FaBars } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-  return <h2>Navbar</h2>
+  return (
+    <Nav>
+      <div className='nav-center'>
+        <div className='logo'>
+          <Link to='/'>
+            <h3>comfort</h3>
+          </Link>
+          <button className='nav-toggle'>
+            <FaBars />
+          </button>
+        </div>
+        <ul className='nav-links'>
+          <li>number1</li>
+          <li>number2</li>
+        </ul>
+        <div className='cart-buttons'>
+          <button>cart</button>
+          <button>login</button>
+        </div>
+      </div>
+    </Nav>
+  )
 }
+
+const Nav = styled.div`
+  height: 7rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: var(--background);
+  .nav-links {
+    display: none;
+  }
+  .cart-buttons {
+    display: none;
+  }
+  .logo {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    h3 {
+      margin-bottom: 0;
+    }
+  }
+  @media (min-width: 992px) {
+    .nav-toggle {
+      display: none;
+    }
+    .nav-center {
+      display: grid;
+      grid-template-columns: auto 1fr auto;
+    }
+    .nav-links {
+      display: flex;
+      justify-content: center;
+      li {
+        margin: 0 0.5rem;
+        color: var(--heading);
+      }
+    }
+    .cart-buttons {
+      display: grid;
+    }
+  }
+  .nav-center {
+    width: 80vw;
+    max-width: 1170px;
+    margin: 0 auto;
+  }
+`
 
 export default Navbar
