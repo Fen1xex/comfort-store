d16f6cce30650964deb2fd258dd0d409dfbd8b9d
KIRILL F <tiltlessman@gmail.com>
2022-03-23 12:53:42 +0300

    cart: add to cart existing item functionality

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -1,22 +1,33 @@
 const cart_reducer = (state, action) => {
   if (action.type === 'ADD_TO_CART') {
     const { id, color, amount, product } = action.payload
-    console.log(product)
     const tempProduct = state.cart.find((item) => item.id === id + color)
     if (tempProduct) {
+      const tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === id + color) {
+          let newAmount = cartItem.amount + amount
+          if (newAmount > cartItem.max) {
+            newAmount = cartItem.max
+          }
+          return { ...cartItem, amount: newAmount }
+        } else {
+          return cartItem
+        }
+      })
+
+      return { ...state, cart: tempCart }
     } else {
       const newItem = {
         id: id + color,
         image: product.images[0].url,
-        color: color,
+        color,
         name: product.name,
         price: product.price,
+        amount,
         max: product.stock,
       }
       return { ...state, cart: [...state.cart, newItem] }
     }
-
-    return { ...state }
   }
   throw new Error(`there is no matching "${action.type}"`)
 }
